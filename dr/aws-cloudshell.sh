eff83ac4df6cbf7eac9b4d800899850253fdc21b
Rafael Gomes <234266+rafaelgom3s@users.noreply.github.com>
2023-05-29 18:19:40 +0100

    chore: updates aws-cloudshell.sh DR script

--- dr/aws-cloudshell.sh
+++ dr/aws-cloudshell.sh
@@ -2,8 +2,17 @@
 
 AWS_REGION=eu-central-1
 VERSION_KUBECTL=1.25.9
-PLATFORM=amd64
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
 
-curl -sS "https://webi.sh/k9s" | sh \
-  && curl -sS "https://webi.sh/kubectx" | sh \
-  && curl -O "https://s3.${AWS_REGION}.amazonaws.com/amazon-eks/${VERSION_KUBECTL}/2023-05-11/bin/linux/${PLATFORM}/kubectl"
+curl -sS "https://webi.sh/k9s" | sh
+curl -sS "https://webi.sh/kubectx" | sh
+
+# https://docs.aws.amazon.com/eks/latest/userguide/install-kubectl.html
+curl -sLO "https://s3.${AWS_REGION}.amazonaws.com/amazon-eks/${VERSION_KUBECTL}/2023-05-11/bin/linux/${PLATFORM}/kubectl"
+
+# https://github.com/weaveworks/eksctl/blob/main/README.md#for-unix
+curl -sLO "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+curl -sL "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check # (Optional) Verify checksum
+tar -xzf "eksctl_${PLATFORM}.tar.gz" -C /tmp && rm "eksctl_${PLATFORM}.tar.gz"
+mv /tmp/eksctl ~/eksctl
