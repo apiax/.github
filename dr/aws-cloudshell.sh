480aaecdc9c638806e8f0d64da084e8d012097dc
Rafael Gomes <234266+rafaelgom3s@users.noreply.github.com>
2023-05-30 11:12:09 +0100

    chore: update aws-cloudshell.sh

--- dr/aws-cloudshell.sh
+++ dr/aws-cloudshell.sh
@@ -3,15 +3,19 @@
 AWS_REGION=eu-central-1
 ARCH=amd64
 PLATFORM=$(uname -s)_${ARCH}
-# VERSION_KUBECTL=1.25.9
+VERSION_KUBECTL=1.25.9
 
 curl -sS https://webi.sh/k9s | sh
 curl -sS https://webi.sh/kubectx | sh
 # curl -sS https://webi.sh/vim-essentials | sh
 
 # https://docs.aws.amazon.com/eks/latest/userguide/install-kubectl.html
-# curl -sLO "https://s3.${AWS_REGION}.amazonaws.com/amazon-eks/${VERSION_KUBECTL}/2023-05-11/bin/linux/${ARCH}/kubectl"
-# mv kubectl ~/.local/bin/kubectl
+curl -sLO "https://s3.${AWS_REGION}.amazonaws.com/amazon-eks/${VERSION_KUBECTL}/2023-05-11/bin/linux/${ARCH}/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+echo 'source <(kubectl completion bash)' >>~/.bashrc
+echo 'alias k=kubectl' >>~/.bashrc
+echo 'complete -o default -F __start_kubectl k' >>~/.bashrc
+source ~/.bashrc
 
 # https://github.com/weaveworks/eksctl/blob/main/README.md#for-unix
 curl -sLO "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_${PLATFORM}.tar.gz"
@@ -21,4 +25,5 @@ mv /tmp/eksctl ~/.local/bin/eksctl
 
 eksctl utils write-kubeconfig --region="${AWS_REGION}" --cluster=dev
 eksctl utils write-kubeconfig --region="${AWS_REGION}" --cluster=qa
+eksctl utils write-kubeconfig --region=eu-west-1 --cluster=pre-production
 eksctl utils write-kubeconfig --region="${AWS_REGION}" --cluster=production
